16e3894562079a323d80df061cdbf51a5cf9bd32
Jordan Hunnicutt <jordanneilhunnicutt@gmail.com>
2023-05-23 06:10:09 -0400

    feat: initial site architecture

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,40 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
+import Head from 'next/head';
+import Link from 'next/link';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import { getSortedPostsData } from '../lib/posts';
+import Date from '../components/date';
 
-export default function Home() {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-    </Layout>
-  )
+export async function getStaticProps() {
+	const allPostsData = getSortedPostsData();
+	return {
+		props: {
+			allPostsData,
+		},
+	};
+}
+
+export default function Home({ allPostsData }) {
+	return (
+		<Layout home>
+			<Head>
+				<title>{siteTitle}</title>
+			</Head>
+			<section className={utilStyles.headingMd}>
+				<p>I'm teaching myself Next.js! This is my blog documenting that.</p>
+			</section>
+			<section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+				<h1 className={`${utilStyles.headingLg}`}>Blog</h1>
+				<ul className={`${utilStyles.list}`}>
+					{allPostsData.map(({ id, date, title }) => (
+						<li className={`${utilStyles.listItem}`} key={id}>
+							<Link href={`/posts/${id}`}>{title}</Link>
+							<br />
+							<small className={utilStyles.lightText}>{date}</small>
+						</li>
+					))}
+				</ul>
+			</section>
+		</Layout>
+	);
 }
